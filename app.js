3532047a6d3a76aa2daf8ea2215a2773ea44bbcd
TeamTaroFTC <137839983+TeamTaro@users.noreply.github.com>
2023-09-07 23:24:51 -0700

    Update app.js

--- app.js
+++ app.js
@@ -12,3 +12,25 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+document.querySelector("a").onmouseover = event => {
+    let iterations = 0;
+    const interval = setInterval(() => {
+        event.target.innerText =event.target.innerText.split("")
+        .map((letter, index) => {
+            if(index < iterations) {
+                return event.target.dataset.value[index];
+            }
+            
+            return letters[Math.floor(Math.random() * 26)]
+        })
+        .join("");
+
+        if(iterations >= event.target.dataset.value.length) clearInterval(interval);
+
+        iterations +=  1;
+    }, 30);
+}
